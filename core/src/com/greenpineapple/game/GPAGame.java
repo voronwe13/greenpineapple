e2ba73ec50743c93e223ea57645bf88b1855c1a5
Alex Kayser <akayser2@gmu.edu>
2016-03-05 01:42:24 -0500

    Created new character class to get robbers and guards

--- core/src/com/greenpineapple/game/GPAGame.java
+++ core/src/com/greenpineapple/game/GPAGame.java
@@ -8,15 +8,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.greenpineapple.input.GPAInputProcessor;
 
 public class GPAGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	private Texture guard; //the guard graphic
-	private Texture robber; //the robber graphic
-	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		guard = new Texture(Gdx.files.internal("Circle.png"));
-		robber = new Texture(Gdx.files.internal("Square.png"));
 		GPAInputProcessor inputProcessor = new GPAInputProcessor();
 		Gdx.input.setInputProcessor(inputProcessor);
 	}
@@ -24,9 +17,5 @@ public class GPAGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
-        batch.begin();
-        batch.draw(guard, 10, 10);
-        batch.draw(robber, 300, 300);
-        batch.end();
 	}
 }
