04b748a42f685737915c60cf327c5033a5948881
Roger Alessi <roger.alessi@gmail.com>
2016-03-05 01:18:47 -0500

    added chat module to test networking

--- desktop/src/com/greenpineapple/game/desktop/DesktopLauncher.java
+++ desktop/src/com/greenpineapple/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.greenpineapple.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.greenpineapple.game.GPAGame;
+import com.greenpineapple.net.chat.ChatModule;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GPAGame(), config);
+		new LwjglApplication(new ChatModule(), config);
 	}
 }
