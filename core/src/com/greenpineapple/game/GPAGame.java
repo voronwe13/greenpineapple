e7cb97bb47ee63d194eefb55cd50dc2fc08f6eb3
Alex Kayser <akayser2@gmu.edu>
2016-03-05 15:49:33 -0500

    MapClass

--- core/src/com/greenpineapple/game/GPAGame.java
+++ core/src/com/greenpineapple/game/GPAGame.java
@@ -2,20 +2,27 @@ package com.greenpineapple.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.greenpineapple.input.GPAInputProcessor;
 
 public class GPAGame extends ApplicationAdapter {
+	
+	String[] lines;
+	
 	@Override
 	public void create () {
 		GPAInputProcessor inputProcessor = new GPAInputProcessor();
 		Gdx.input.setInputProcessor(inputProcessor);
+		FileHandle file = Gdx.files.internal("MapTest.txt");
+		String text = file.readString();
+		lines = text.split("\n");
+		System.out.println(lines.toString());
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
+		
 	}
 }
