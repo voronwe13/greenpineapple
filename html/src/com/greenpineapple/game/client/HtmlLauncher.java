77b02dd3b03f7819d03d50c97c981a336d7a7868
Peter Schatz <voronwe13@gmail.com>
2016-03-04 22:40:13 -0500

    New project files

---
+++ html/src/com/greenpineapple/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.greenpineapple.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.greenpineapple.game.GPAGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GPAGame();
+        }
+}
