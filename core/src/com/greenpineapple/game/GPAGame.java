fe44e00c8c1ab3c94e3002fffaf75288670a79ae
Alex Kayser <akayser2@gmu.edu>
2016-03-05 00:24:04 -0500

    Shapes for graphics

--- core/src/com/greenpineapple/game/GPAGame.java
+++ core/src/com/greenpineapple/game/GPAGame.java
@@ -8,20 +8,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GPAGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	private Texture guard; //the guard graphic
+	private Texture robber; //the robber graphic
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		guard = new Texture(Gdx.files.internal("Circle.png"));
+		robber = new Texture(Gdx.files.internal("Square.png"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
+        batch.begin();
+        batch.draw(guard, 10, 10);
+        batch.draw(robber, 300, 300);
+        batch.end();
 	}
 }
