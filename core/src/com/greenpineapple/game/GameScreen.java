9f6fc54419fda54c8cffaa1a1834b6332a6e18ca
Roger Alessi <roger.alessi@gmail.com>
2016-03-06 14:01:54 -0500

    formatted

--- core/src/com/greenpineapple/game/GameScreen.java
+++ core/src/com/greenpineapple/game/GameScreen.java
@@ -36,18 +36,18 @@ public class GameScreen implements Screen {
 	private String[] lines;
 
 	private SpriteBatch batch;
-	//private GPAPlayer guardplayer, robberplayer;
+	// private GPAPlayer guardplayer, robberplayer;
 	private List<GPAPlayer> guardplayers, robberplayers;
 	private List<Treasure> treasures;
-	
-    private Texture mapimage;
-    private TextureRegion   guardcurrentframe, robbercurrentframe;
 
-    float statetime;
-    
-    MapClass map;
-	
-    public GameScreen(Game game) {
+	private Texture mapimage;
+	private TextureRegion guardcurrentframe, robbercurrentframe;
+
+	float statetime;
+
+	MapClass map;
+
+	public GameScreen(Game game) {
 		batch = new SpriteBatch();
 		map = new MapClass();
 		List<Vector2> guardpositions = map.getGuardPositions();
@@ -56,25 +56,25 @@ public class GameScreen implements Screen {
 		guardplayers = new ArrayList<GPAPlayer>();
 		robberplayers = new ArrayList<GPAPlayer>();
 		treasures = new ArrayList<Treasure>();
-		for(int i=0; i<guardpositions.size(); i++){
+		for (int i = 0; i < guardpositions.size(); i++) {
 			GPAPlayer guardplayer = new GPAPlayer(PlayerType.GUARD);
 			guardplayer.setTexture("Guard Sprite polished small.png");
-			int posx = (int)guardpositions.get(i).x;
-			int posy = (int)guardpositions.get(i).y;
+			int posx = (int) guardpositions.get(i).x;
+			int posy = (int) guardpositions.get(i).y;
 			guardplayer.setPosition(posx, posy);
 			guardplayer.setMap(map);
 			guardplayers.add(guardplayer);
 		}
-		for(int i=0; i<robberpositions.size(); i++){
+		for (int i = 0; i < robberpositions.size(); i++) {
 			GPAPlayer robberplayer = new GPAPlayer(PlayerType.ROBBER);
 			robberplayer.setTexture("Thief Sprite polished small.png");
-			int posx = (int)robberpositions.get(i).x;
-			int posy = (int)robberpositions.get(i).y;
+			int posx = (int) robberpositions.get(i).x;
+			int posy = (int) robberpositions.get(i).y;
 			robberplayer.setPosition(posx, posy);
 			robberplayer.setMap(map);
 			robberplayers.add(robberplayer);
 		}
-		for(Vector2 treasureposition:treasurepositions){
+		for (Vector2 treasureposition : treasurepositions) {
 			Treasure treasure = new Treasure(treasureposition);
 			treasures.add(treasure);
 		}
@@ -85,39 +85,40 @@ public class GameScreen implements Screen {
 		RayHandler.useDiffuseLight(true);
 		rayhandler = new RayHandler(world);
 		rayhandler.setCulling(true);
-		//rayhandler.setCombinedMatrix(gameController.camera.combined);
+		// rayhandler.setCombinedMatrix(gameController.camera.combined);
 		rayhandler.setAmbientLight(1);
 		rayhandler.setShadows(false);
-		conelight = new ConeLight(rayhandler, RAYS_NUM, new Color(.1f,.1f,.5f,.5f), 5f, 20, 20, 20, 20);
+		conelight = new ConeLight(rayhandler, RAYS_NUM, new Color(.1f, .1f, .5f, .5f), 5f, 20, 20, 20, 20);
 
 		statetime = 0f;
 	}
-    
+
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void render(float delta) {
-		for(GPAPlayer guardplayer:guardplayers){
+		for (GPAPlayer guardplayer : guardplayers) {
 			guardplayer.update();
 		}
-		for(GPAPlayer robberplayer:robberplayers){
+		for (GPAPlayer robberplayer : robberplayers) {
 			robberplayer.update();
 		}
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears
+													// the screen.
 		statetime += Gdx.graphics.getDeltaTime();
-        mapimage = map.getImage();
-        batch.begin();
-        batch.draw(mapimage, 0, 0);
-		for(GPAPlayer guardplayer:guardplayers){
-	        guardcurrentframe = guardplayer.getCurrentFrame(statetime);
-	        batch.draw(guardcurrentframe, guardplayer.getPositionX(), guardplayer.getPositionY());
+		mapimage = map.getImage();
+		batch.begin();
+		batch.draw(mapimage, 0, 0);
+		for (GPAPlayer guardplayer : guardplayers) {
+			guardcurrentframe = guardplayer.getCurrentFrame(statetime);
+			batch.draw(guardcurrentframe, guardplayer.getPositionX(), guardplayer.getPositionY());
 		}
-		for(GPAPlayer robberplayer:robberplayers){
-	        robbercurrentframe = robberplayer.getCurrentFrame(statetime);
+		for (GPAPlayer robberplayer : robberplayers) {
+			robbercurrentframe = robberplayer.getCurrentFrame(statetime);
 			batch.draw(robbercurrentframe, robberplayer.getPositionX(), robberplayer.getPositionY());
 		}
 		batch.end();
@@ -131,31 +132,31 @@ public class GameScreen implements Screen {
 		cam = new OrthographicCamera(20.0f, 20.0f * height / width);
 		cam.position.set(cam.viewportWidth / 2.0f, cam.viewportHeight / 2.0f, 0.0f);
 		cam.update();
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
