903ff271c980d39483cc3ef2df15c75d263cf6d2
Peter Schatz <voronwe13@gmail.com>
2016-03-06 14:42:19 -0500

    Added to sound class.  Some are still stubs, and there are some things still missing from it.

--- core/src/com/greenpineapple/sound/GPASound.java
+++ core/src/com/greenpineapple/sound/GPASound.java
@@ -11,14 +11,44 @@ public class GPASound {
 	private Map<String, Music> songs;
 	private float soundeffectvolume = 1.0f, musicvolume = 1.0f;
 	
+	private static GPASound soundinstance = null;
+
 	public void playSound(String soundname){
 		soundeffects.get(soundname).play(soundeffectvolume);
+	}
+	
+	public void stopSound(String soundname){
+		soundeffects.get(soundname).stop();
 	}
 	
 	public void playSong(String songname){
 		songs.get(songname).play();
 	}
 	
+	public void stopSong(String songname){
+		songs.get(songname).stop();
+	}
 	
+	public static GPASound getInstance(){
+		if(soundinstance == null)
+			soundinstance = new GPASound();
+		return soundinstance;
+	}
+	
+	public void addSoundEffect(String soundpath, String soundname){
+		
+	}
+	
+	public void addSong(String songpath, String songname){
+		
+	}
+
+	public void setSoundEffectVolume(float volume){
+		
+	}
+	
+	public void setMusicVolume(float volume){
+		
+	}
 	
 }
